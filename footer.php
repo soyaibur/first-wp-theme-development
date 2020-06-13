70197a6d3a82f673cdaedb3ec938d00b48528c75
soyaibur <ovisoyaiburrahman@gmail.com>
2020-06-13 21:30:21 +0600

    until Css file adding by creating functions.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="site-footer">
+  <?php  bloginfo('name'); ?>
+</footer>
+<?php wp_footer(); ?>
+
+
+
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html <?php language_attributes(); ?>>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo('charset');  ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+
+
+<!--site header -->
+<header class="site-header">
+ <h1><a href="<?php  echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+ <h5><?php bloginfo('description'); ?></h5>
+</header><!--/site header -->
